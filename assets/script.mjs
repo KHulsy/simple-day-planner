be14e2066538b811ec01a9a3a14557d62e034c8c
lmoody <l.moody@continuumonline.com>
2019-12-22 21:34:44 -0700

    fixed loading order for localstorage

--- assets/script.mjs
+++ assets/script.mjs
@@ -2,105 +2,105 @@
 
 var myDay = [
     {
-        id: 0,
+        id: "0",
         hour: "09",
         time: "09",
         meridiem: "am",
         reminder: ""
     },
     {
-        id: 1,
+        id: "1",
         hour: "10",
         time: "10",
         meridiem: "am",
         reminder: ""
     },
     {
-        id: 2,
+        id: "2",
         hour: "11",
         time: "11",
         meridiem: "am",
         reminder: ""
     },
     {
-        id: 3,
+        id: "3",
         hour: "12",
         time: "12",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 4,
+        id: "4",
         hour: "01",
         time: "13",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 5,
+        id: "5",
         hour: "02",
         time: "14",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 6,
+        id: "6",
         hour: "03",
         time: "15",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 7,
+        id: "7",
         hour: "04",
         time: "16",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 8,
+        id: "8",
         hour: "05",
         time: "17",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 9,
+        id: "9",
         hour: "06",
         time: "18",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 10,
+        id: "10",
         hour: "07",
         time: "19",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 11,
+        id: "11",
         hour: "08",
         time: "20",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 12,
+        id: "12",
         hour: "09",
         time: "21",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 13,
+        id: "13",
         hour: "10",
         time: "22",
         meridiem: "pm",
         reminder: ""
     },
     {
-        id: 14,
+        id: "14",
         hour: "11",
         time: "23",
         meridiem: "pm",
@@ -135,7 +135,7 @@ function init() {
     displayReminders();
 }
 
-init();
+
 
 getHeaderDate();
 
@@ -185,12 +185,13 @@ myDay.forEach(function(thisHour) {
     hourRow.append(hourField, hourPlan, savePlan);
 })
 
+init();
 
 $(".saveBtn").on("click", function(event) {
     event.preventDefault();
     var saveIndex = $(this).siblings(".description").children(".future").attr("id");
     myDay[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
-    
+    console.log(saveIndex);
     saveReminders();
     displayReminders();
 })
