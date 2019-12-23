cdda0defa1b50e850877851af98afa8549e42ccd
lmoody <l.moody@continuumonline.com>
2019-12-22 21:26:44 -0700

    working on local storage

--- assets/calendar.mjs
+++ assets/calendar.mjs
@@ -1,77 +1,107 @@
 export var myDay = [
     {
+        id: 0, 
         hour: "09",
         time: "09",
-        meridiem: "am"
+        meridiem: "am",
+        reminder: ""
     },
     {
+        id: 1, 
         hour: "10",
         time: "10",
-        meridiem: "am"
+        meridiem: "am",
+        reminder: ""
     },
     {
+        id: 2, 
         hour: "11",
         time: "11",
-        meridiem: "am"
+        meridiem: "am",
+        reminder: ""
     },
     {
+        id: 3, 
         hour: "12",
         time: "12",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 4, 
         hour: "01",
         time: "13",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 5, 
         hour: "02",
         time: "14",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 6, 
         hour: "03",
         time: "15",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 7, 
         hour: "04",
         time: "16",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 8, 
         hour: "05",
         time: "17",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 9, 
         hour: "06",
         time: "18",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 10, 
         hour: "07",
         time: "19",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 11, 
         hour: "08",
         time: "20",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 12, 
         hour: "09",
         time: "21",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 13, 
         hour: "10",
         time: "22",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     },
     {
+        id: 14, 
         hour: "11",
         time: "23",
-        meridiem: "pm"
+        meridiem: "pm",
+        reminder: ""
     }
 ]
--- assets/script.mjs
+++ assets/script.mjs
@@ -1,20 +1,147 @@
-import { myDay } from "./calendar.mjs";
+// import { myDay } from "./calendar.mjs";
 
-// create a loop for the current day by hour
-// create place to store data within each hours block
-// add functionality to save each hours info to local storage
+var myDay = [
+    {
+        id: 0,
+        hour: "09",
+        time: "09",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: 1,
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: 2,
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: 3,
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 4,
+        hour: "01",
+        time: "13",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 5,
+        hour: "02",
+        time: "14",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 6,
+        hour: "03",
+        time: "15",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 7,
+        hour: "04",
+        time: "16",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 8,
+        hour: "05",
+        time: "17",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 9,
+        hour: "06",
+        time: "18",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 10,
+        hour: "07",
+        time: "19",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 11,
+        hour: "08",
+        time: "20",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 12,
+        hour: "09",
+        time: "21",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 13,
+        hour: "10",
+        time: "22",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: 14,
+        hour: "11",
+        time: "23",
+        meridiem: "pm",
+        reminder: ""
+    }
+]
+
 
 function getHeaderDate() {
-    var currentHeaderDate = moment().format('dddd, MMMM YYYY');
+    var currentHeaderDate = moment().format('dddd, MMMM Do');
     $("#currentDay").text(currentHeaderDate);
 }
+
+function saveReminders() {
+    localStorage.setItem("myDay", JSON.stringify(myDay));
+}
+
+function displayReminders() {
+    myDay.forEach(function (_thisHour) {
+        $(`#${_thisHour.id}`).val(_thisHour.reminder);
+    })
+}
+
+function init() {
+    var storedDay = JSON.parse(localStorage.getItem("myDay"));
+
+    if (storedDay) {
+        myDay = storedDay;
+    }
+
+    saveReminders();
+    displayReminders();
+}
+
+init();
 
 getHeaderDate();
 
 myDay.forEach(function(thisHour) {
 
-    var hourRow = $("<div>").attr({
-        "id": `${thisHour.hour}`,
+    var hourRow = $("<form>").attr({
         "class": "row"
     });
     $(".container").append(hourRow);
@@ -33,9 +160,10 @@ myDay.forEach(function(thisHour) {
         });
     var planData = $("<textarea>");
     hourPlan.append(planData);
+    planData.attr("id", thisHour.id);
     if (thisHour.time < moment().format("HH")) {
         planData.attr ({
-            "class": "past"
+            "class": "past", 
         })
     } else if (thisHour.time === moment().format("HH")) {
         planData.attr({
@@ -49,12 +177,20 @@ myDay.forEach(function(thisHour) {
 
 
     var saveButton = $("<i class='far fa-save fa-lg'></i>")
-    var savePlan = $("<div>")
+    var savePlan = $("<button>")
         .attr({
-            "class": "col-md-1 saveBtn pt-4"
+            "class": "col-md-1 saveBtn"
     });
-
-
     savePlan.append(saveButton);
     hourRow.append(hourField, hourPlan, savePlan);
+})
+
+
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
+    myDay[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
+    
+    saveReminders();
+    displayReminders();
 })
